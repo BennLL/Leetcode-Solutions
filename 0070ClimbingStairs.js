41a7f06a21a68db3d2d838bee6b0215586eaed36
BennLL <zihaoluo153@gmail.com>
2022-09-08 14:51:10 -0700

    attempted 0070

--- 0070ClimbingStairs.js
+++ 0070ClimbingStairs.js
@@ -10,3 +10,15 @@ n = 3;
 console.log(climbStairs(n));
 
 // try permutation or combination
+// var isPalindrome = function(s) {
+//     x = s.replace(/[^A-Za-z0-9]/g, '');
+//     x = x.toLowerCase();
+//     y = x.split("").reverse().join("");
+//     if (x == y){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+//     return x;
+// };
