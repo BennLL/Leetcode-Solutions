002a46f59047e26fbea02ad14b0436fd03b7f46c
BennLL <zihaoluo153@gmail.com>
2022-09-25 17:24:24 -0700

    completed

--- 0021MergeTwoSortedLists.js
+++ 0021MergeTwoSortedLists.js
@@ -1,30 +1,118 @@
-//doesn't work
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
 var mergeTwoLists = function(list1, list2) {
-    
-    for(x = 0; x < list2.length; x++){
-        list1.push(list2[x])
+    let current = new ListNode();
+    let headofcurrent = current;
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        }else{
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
     }
     
-    list1.sort(function(a, b){return a - b});
-    return list1
+    
+    if(list1){
+        current.next = list1;
+        list1 = list1.next;
+        current = current.next;
+    }
+    
+    if(list2){
+        current.next = list2;
+        list2 = list2.next;
+        current = current.next;
+    }
+    
+    return headofcurrent.next;
 };
 
-list1 = [52,3,6], list2 = [6,7,1,2]
-console.log(mergeTwoLists(list1, list2))
+// // used to test 
 
-/*
-Eample 1:
-Input: list1 = [1,2,4], list2 = [1,3,4]
-Output: [1,1,2,3,4,4]
 
-Example 2:
-Input: list1 = [], list2 = []
-Output: []
+// var mergeTwoLists = function(list1, list2) {
+//     let current = new Node();
+//     let headofcurrent = current;
+    
+//     while(list1 && list2){
+//         if(list1.data <list2.data){
+//             current.next = list1;
+//             list1 = list1.next;
+//         }else{
+//             current.next = list2;
+//             list2 = list2.next;
+//         }
+//         current = current.next;
+//     }
 
-Example 3:
-Input: list1 = [], list2 = [0]
-Output: [0]
+//     if(list1){
+//         current.next = list1;
+//         list1 = list1.next;
+//         current = current.next;
+//     }
+//     if(list2){
+//         current.next = list2;
+//         list2 = list2.next;
+//         current = current.next;
+//     }
+//     printLinkedList(current);
+//     return headofcurrent.next;
+// };
 
-list1 = [52,3,6], list2 = [6,7,1,2]
-*/
-//not completed
+// class Node{
+//     constructor(data){
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// function print(){
+//     while(head){
+//         console.log(head.value);
+//         head = head.next;
+//     }
+// }
+
+// const printLinkedList = (head) => {
+//     let current = head;
+//     while(current != null){
+//         console.log(current.data);
+//         current = current.next;
+//     }
+// }
+
+
+
+// var a = new Node(1);
+// var b = new Node(2);
+// var c = new Node(4);
+
+// a.next = b;
+// b.next = c;
+
+// var one = new Node(1);
+// var two = new Node(3);
+// var three = new Node(4);
+
+// one.next = two;
+// two.next = three;
+
+
+// // printLinkedList(a);
+// // console.log("--------------");
+// // printLinkedList(one);
+// console.log("--------------");
+// console.log(mergeTwoLists(a , one));
--- test.js
+++ test.js
@@ -1,4 +1,76 @@
-for(x = 0; x < 10; x ++ ){
-    console.log(x)
+// used to test 
+
+
+var mergeTwoLists = function(list1, list2) {
+    let current = new Node();
+    let headofcurrent = current;
+    
+    while(list1 && list2){
+        if(list1.data <list2.data){
+            current.next = list1;
+            list1 = list1.next;
+        }else{
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+
+    if(list1){
+        current.next = list1;
+        list1 = list1.next;
+        current = current.next;
+    }
+    if(list2){
+        current.next = list2;
+        list2 = list2.next;
+        current = current.next;
+    }
+    printLinkedList(current);
+    return headofcurrent.next;
+};
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
 }
-//update
+
+function print(){
+    while(head){
+        console.log(head.value);
+        head = head.next;
+    }
+}
+
+const printLinkedList = (head) => {
+    let current = head;
+    while(current != null){
+        console.log(current.data);
+        current = current.next;
+    }
+}
+
+
+
+var a = new Node(1);
+var b = new Node(2);
+var c = new Node(4);
+
+a.next = b;
+b.next = c;
+
+var one = new Node(1);
+var two = new Node(3);
+var three = new Node(4);
+
+one.next = two;
+two.next = three;
+
+
+// printLinkedList(a);
+// console.log("--------------");
+// printLinkedList(one);
+console.log("--------------");
+console.log(mergeTwoLists(a , one));
