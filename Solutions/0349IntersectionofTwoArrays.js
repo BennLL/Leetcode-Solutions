67021b79188a5f737c1d3a104370f9b2d88fd49f
BenLL <zihaoluo153@gmail.com>
2023-08-04 16:12:43 -0700

    completed

---
+++ Solutions/0349IntersectionofTwoArrays.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    nums1.sort(function(a,b){return a-b})
+    nums2.sort(function(a,b){return a-b})
+    same = []
+    n1 = 0;
+    n2 = 0;
+    while(n1 < nums1.length && n2 < nums2.length){
+        if(nums1[n1] == nums2[n2]){
+            same.push(nums1[n1]);
+            n1++;
+            n2++;
+        }else if(nums1[n1] < nums2[n2]){
+            n1++;
+        }else if(nums1[n1] > nums2[n2]){
+            n2++;
+        }
+    }
+    same = Array.from(new Set(same));
+    return same;
+};
+
